e1324d5c8614b3c370a9b60a25ef9b2ba2870648
Waldir Leoncio <waldir.leoncio@gmail.com>
2021-03-09 12:16:58 +0100

    Improved user feedback

--- contingencytables/disintegrate.sh
+++ contingencytables/disintegrate.sh
@@ -3,14 +3,15 @@
 
 feature_branch=$(git branch --show-current)
 
+echo "# Branch switching\n"
 echo "On branch $feature_branch. Switching to develop"
 git checkout develop
 
-echo -e "\nMerging $feature_branch into develop"
+echo -e "\n# Merging $feature_branch into develop\n"
 echo -en "Enter chapter number: "
 read chap_num
 git merge $feature_branch -m "Merged $feature_branch with develop (#$chap_num)"
 git branch --delete $feature_branch
 
-echo -e "\n Git log"
+echo -e "\n# Updated git log\n"
 git log --graph --pretty=format:'%C(yellow)%d%Creset %s %Cgreen(%cr)' -n 10
