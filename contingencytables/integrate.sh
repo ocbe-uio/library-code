438580fe7378cb27f1b0a6e2f19cd8fc6472dc30
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-06-21 10:29:07 +0200

    Added validation step

--- contingencytables/integrate.sh
+++ contingencytables/integrate.sh
@@ -1,6 +1,14 @@
 #! /bin/bash
 # This script automates the preparation of a script file for integrating the package.
 
+# Validation
+
+if [ -z $1 ]
+then
+	echo 'Error: you must provide a file path. Maybe you mean to run disintegrate.sh?'
+	exit 0
+fi
+
 # ======================================================== #
 # Moving file to package                                   #
 # ======================================================== #
@@ -34,5 +42,14 @@ echo $filename
 echo -e "\nFunction name copied to clipboard. Opening file in editor..."
 xdg-open R/$filename_ext
 
-echo "Run the following in R to reformat the function:"
-echo "contingencytables:::reformat('R/$filename_ext')"
+echo -en "Reformat script [y/N]? "
+read reformat_yn
+if [ "$reformat_yn" = 'y' ]
+then
+	echo -e "\nReformatting. If this goes wrong, you can reformat the file manually with:"
+	echo "reformat.sh R/$filename_ext"
+	reformat.sh R/$filename_ext
+else
+	echo "Whenever you are ready, run the following to reformat the function:"
+	echo "reformat.sh R/$filename_ext"
+fi
--- contingencytables/reformat.sh
+++ contingencytables/reformat.sh
@@ -3,9 +3,25 @@
 SCRIPT_PATH=~/UiO/rse-tools/contingencytables
 
 rewrite=TRUE
+valid_chapter_number='^[0-9]+$'
+ch_num=0
+
 if [ $2 = false ] || [ $2 = FALSE ]
 then
 	rewrite=FALSE
+	if [[ $3 =~ $valid_chapter_number ]]; then
+		ch_num=$3
+	else
+		echo "No chapter number provided. I'll try to estimate it from the code"
+	fi
+elif [[ $2 =~ $valid_chapter_number ]]; then
+		ch_num=$2
+else
+	echo "No chapter number provided. I'll try to estimate it from the code"
 fi
 
-R -e "source('$SCRIPT_PATH/reformat.R'); reformat('$1', $rewrite)"
+if [ $ch_num -eq 0 ]; then
+	R -e "source('$SCRIPT_PATH/reformat.R'); reformat('$1', saveOutput=$rewrite)"
+else
+	R -e "source('$SCRIPT_PATH/reformat.R'); reformat('$1', $ch_num, $rewrite)"
+fi
