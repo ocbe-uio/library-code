2cb56411e00f9955847eb905927fbfa4902e5a45
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-06-21 10:27:43 +0200

    Added step to commit changes

--- contingencytables/disintegrate.sh
+++ contingencytables/disintegrate.sh
@@ -3,7 +3,28 @@
 
 feature_branch=$(git branch --show-current)
 
-echo "# Branch switching\n"
+# Commiting changes
+echo -e "# Pending changes"
+git status
+echo -e "Proceed with commits [Y/n]?"
+read proceed_yn
+if [ $proceed_yn = 'n' ]
+then
+	echo "Exiting"
+	exit 0
+fi
+
+echo -e "# Commiting changes"
+git add R/$feature_branch.R
+git commit -m "Reformatted function"
+git add R/chapters.R
+git commit -m "Added function to chapter list"
+git add tests/testthat/*
+git commit -m "Added unit tests"
+git add man/$feature_branch.Rd
+git commit -m "Updated docs"
+
+echo -e "# Branch switching\n"
 echo "On branch $feature_branch. Switching to develop"
 git checkout develop
 
