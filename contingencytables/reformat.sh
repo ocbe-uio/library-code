b022eece6bcf7125a96226cfdd08100352171bc6
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-10-20 14:13:06 +0200

    Improved handling of chapter numbers

--- contingencytables/reformat.sh
+++ contingencytables/reformat.sh
@@ -5,19 +5,21 @@ SCRIPT_PATH=~/UiO/rse-tools/contingencytables
 rewrite=TRUE
 valid_chapter_number='^[0-9]+$'
 ch_num=0
-
-if [ $2 = false ] || [ $2 = FALSE ]
+if [ -z $2 ]
 then
-	rewrite=FALSE
-	if [[ $3 =~ $valid_chapter_number ]]; then
-		ch_num=$3
+	if [ $2 = false ] || [ $2 = FALSE ]
+	then
+		rewrite=FALSE
+		if [[ $3 =~ $valid_chapter_number ]]; then
+			ch_num=$3
+		else
+			echo "No chapter number provided. I'll try to estimate it from the code"
+		fi
+	elif [[ $2 =~ $valid_chapter_number ]]; then
+			ch_num=$2
 	else
-		echo "No chapter number provided. I'll try to estimate it from the code"
+		echo "No chapter number provided. Trying to estimate it from the code"
 	fi
-elif [[ $2 =~ $valid_chapter_number ]]; then
-		ch_num=$2
-else
-	echo "No chapter number provided. I'll try to estimate it from the code"
 fi
 
 if [ $ch_num -eq 0 ]; then
