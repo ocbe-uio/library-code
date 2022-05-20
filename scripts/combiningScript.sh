f7b392b56db2526ff1f4e1820f566e6fdede7733
Waldir Leoncio <w.l.netto@medisin.uio.no>
2022-05-20 09:23:14 +0200

    Added usage prompt

--- scripts/combiningScript.sh
+++ scripts/combiningScript.sh
@@ -3,6 +3,12 @@
 # This script quickly juxtaposes two images using imagemagick
 # Output is made out of 4 files, combining JPG and TIF output with vertical and horizontal juxtapositions.
 
+if [ -z $1 ] || [ -z $2 ]
+then
+  echo "Usage: combiningScript.sh [FILES]"
+  echo "Please provide at least two file names"
+  exit 1
+fi
 convert $* +append out_horizontal.jpg
 convert $* +append out_horizontal.tif
 convert $* +append out_horizontal.pdf
