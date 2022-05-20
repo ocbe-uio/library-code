255c82895843daad983ffbac7b1070dfe2134956
Waldir Leoncio <w.l.netto@medisin.uio.no>
2022-05-20 06:40:50 +0200

    Combining script now accepts 3+ arguments (#3)

--- scripts/combiningScript.sh
+++ scripts/combiningScript.sh
@@ -3,7 +3,7 @@
 # This script quickly juxtaposes two images using imagemagick
 # Output is made out of 4 files, combining JPG and TIF output with vertical and horizontal juxtapositions.
 
-convert $1 $2 +append out_horizontal.jpg
-convert $1 $2 +append out_horizontal.tif
-convert $1 $2 -append out_vertical.jpg
-convert $1 $2 -append out_vertical.tif
+convert $* +append out_horizontal.jpg
+convert $* +append out_horizontal.tif
+convert $* -append out_vertical.jpg
+convert $* -append out_vertical.tif
